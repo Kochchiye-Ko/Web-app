ef64e473d4bf212c780538001ee7f0ce1e4463fc
DilankaNimsara <dilankanimsara101@gmail.com>
2020-05-12 17:22:24 +0530

    google-map component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GoogleMapComponent} from './google-map/google-map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'maps', component: GoogleMapComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+
+import { environment } from '../environments/environment';
+import { AgmCoreModule } from '@agm/core';
+import { AngularFireModule } from 'angularfire2';
+export const firebaseconfig = environment.firebaseConfig;
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseconfig),
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleMapsKey
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
