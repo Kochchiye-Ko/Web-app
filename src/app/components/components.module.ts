b9412bd814efacf808d7f89f4972e8d258b1436b
Isuri Liyanage <36620128+liyanageisu@users.noreply.github.com>
2020-07-17 22:36:32 +0530

    contact page

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,7 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HomeComponent } from "../pages/home/home/home/home.component";
 
 @NgModule({
   imports: [CommonModule, RouterModule, NgbModule],
