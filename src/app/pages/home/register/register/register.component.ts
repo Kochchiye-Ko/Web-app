8486bcc66b809fb2c19b5189666793ec84949dfb
Pawani Imalsha <imalshapawani95@gmail.com>
2020-10-11 01:37:46 +0530

    register part completed

--- src/app/pages/home/register/register/register.component.ts
+++ src/app/pages/home/register/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  emailFormControl = new FormControl("", [Validators.required, Validators.email]);
+  email = "";
+  password="";
+  message = '';    //message give after register
+  errorMessage ='';   //validation error handle
+  error: {name: string, message: string} = { name: '', message: '' }; //for firebase error handle
+
+  getErrorMessage() {
+    if (this.emailFormControl.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.emailFormControl.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  hide = true;
+  constructor(private authservice: AuthService , private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  clearErrorMessage(){
+    this.errorMessage = '';
+    this.error = {name: '' , message: ''};
+  }
+
+  register(){
+    this.clearErrorMessage();
+    if(this.validateForm(this.email , this.password))
+    {
+      this.authservice.registerWithEmail(this.email , this.password)
+      .then(() => {
+        this.message = "You are register with data on firebase"
+        //this.router.navigate(['/userinfo'])
+
+      }).catch(_error => {
+        this.error = _error
+        this.router.navigate(['/register'])
+
+      })
+    }
+  }
+
+  validateForm(email , password){
+    if(email.length === 0){
+      this.errorMessage = "Please enter a valid email";
+      return false;
+    }
+
+    if(password.length === 0){
+      this.errorMessage = "Please enter password";
+      return false;
+    }
+
+    if(password.length < 6){
+      this.errorMessage = "password should be at least 6 characters";
+      return false;
+    }
+
+    this.errorMessage = '';
+    return true;
+  }
 }
