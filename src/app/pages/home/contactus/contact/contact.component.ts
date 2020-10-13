94c5d2278c2de31ea17b3ec4a10007234b849a1e
Isuri Liyanage <36620128+liyanageisu@users.noreply.github.com>
2020-10-13 23:01:27 +0530

    messages 2

--- src/app/pages/home/contactus/contact/contact.component.ts
+++ src/app/pages/home/contactus/contact/contact.component.ts
@@ -21,36 +21,17 @@ export class ContactComponent implements OnInit {
   constructor(private msgService: AuthService,private toster: ToastrService) { }
 
   ngOnInit(){
-    this.resetForm();
-  //   this.msgService.().subscribe(notifications => {
-  //     this.messages = notifications;
-   
-  // })
 }
-  // onSubmit(){
-  //   if (this.addmessages.name != '' && this.addmessages.email != '') {
-  //     this.msgService.addMessages(this.addmessages);
-  //     this.addmessages.name= '';
-  //     this.addmessages.email='';
-  //     this.addmessages.message = '';
-  //     this.toster.success("Sent message successfully", "" + this.addmessages.name);
-  //   }
- // }
-  onsubmit(form: NgForm) {
-    let data = Object.assign({}, form.value);
-    console.log(form.value);  
-    this.msgService.addMessages(data);
-    this.toster.success("Sent message successfully");
-    this.resetForm();
+  onSubmit(){
+    if (this.addmessages.name != '' && this.addmessages.email != '') {
+      this.msgService.addMessages(this.addmessages);
+      this.addmessages.name= '';
+      this.addmessages.email='';
+      this.addmessages.message = '';
+      this.toster.success("Sent message successfully", "" + this.addmessages.name);
+    }
   }
+ 
 
-  resetForm(form?: NgForm) {
-    if (form != null)
-      form.resetForm();
-    this.addmessages = {
-      name: null,
-      email:null,
-      message: null
-     }
-
-    }}
+}
+    
